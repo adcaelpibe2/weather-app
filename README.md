19e5656049d2c38b1580ffd4574384a9e1fd960d
adcaelpibe2 <96590826+adcaelpibe2@users.noreply.github.com>
2025-08-05 22:37:01 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# 🌦️ Weather App
+
+**Weather App** is a simple and responsive weather forecast application that allows users to search for any city in the world and view the weather forecast for the next 7 days. The app is built with HTML, CSS (Flexbox & Grid), and JavaScript, and it fetches real-time weather data using a public weather API.
+
+## 🔍 How It Works
+
+- Type the name of any city in the input field
+- Press **Enter** to search
+- Instantly receive a 7-day weather forecast for that location
+
+## 🛠️ Built With
+
+- HTML5
+- CSS3
+  - Flexbox
+  - CSS Grid
+  - Responsive Design
+- JavaScript (ES6+)
+- Weather API (e.g. OpenWeather API)
+- Git & GitHub
+
+## ✨ Features
+
+- Search for any city worldwide
+- Displays 7-day weather forecast
+- Weather icons, temperature, humidity, and more
+- Clean, intuitive, and mobile-friendly UI
+
+## 📁 Getting Started
+
+To run this project locally:
+
+```bash
+git clone https://github.com/tu-usuario/weather-app.git
+cd weather-app
+open index.html
--- script.js
+++ script.js
@@ -1,65 +1,71 @@
-//var city = ""
 
-    var plus = document.getElementById("form-app");
-    plus.addEventListener("submit", function(e){
-        e.preventDefault()
-        console.log("Hey")
-        var city = ""
-        city = document.querySelector("#first").value
-        const apiKey = 'ed8b08e391d74f66b2c30059804ce319'
-
-            getOne(city , apiKey)
-                .then(data => data.json())
-                .then(users => {
-             
-                    ListUsers(users)
-                   // console.log(users)
-
-                    return getAll(city,apiKey)
-                })
-
-                .then(data=> data.json())
-                .then(users2=>{
-
-                    Current2(users2.list)
-                
-                })
+var plus = document.getElementById("form-app");
+plus.addEventListener("submit", function (e) {
 
 
-    })
+    e.preventDefault()
+    var city = ""
+    city = document.querySelector("#search").value
+    const apiKey = 'ed8b08e391d74f66b2c30059804ce319'
+    
 
-function getOne(city , apiKey){
+    getOne(city, apiKey)
+        .then(data => data.json())
+        .then(users => {
 
-    const CurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+apiKey+'&units=metric';
+            ListUsers(users)
+
+            return getAll(city, apiKey)
+        })
+
+        .then(data => data.json())
+        .then(users2 => {
+
+            Current2(users2.list)
+
+        })
+
+})
+
+
+function getOne(city, apiKey) {
+
+    const CurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=imperial';
     return fetch(CurrentWeather)
-}
-
-function ListUsers(hell){
-
-            var dat = document.querySelector("#date");
-            dat.innerHTML = 
-                "City : " + hell.name + " <br> " +
-                "Description : "+ hell.weather[0].description + "<br>"+
-                "Temperature :  " + hell.main.temp + " °C" + "<br>" +
-                "DETAILS : <br>"+
-                "Feels Like : "+ hell.main.feels_like + "<br>"+
-                "Wind : "  + hell.wind.speed + "<br>"+
-                "Humidity :" + hell.main.humidity + "<br>" +
-                "Pressure : " +  hell.main.pressure
 
 }
 
 
+function ListUsers(hell) {
 
-function getAll(city,apiKey){
+    var iconCode = hell.weather[0].icon;
+    const iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
-    const Forecast = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey+"&units=metric"
+    var dat = document.querySelector("#date");
+    dat.innerHTML =
+
+        `<div class="city"> City   </div><div class="name">`+ hell.name + `</div>`+
+        `<div class="mix"><div class="temp"> `+  Math.round(hell.main.temp) + `°` +`</div>` +`<div class="icon"><img src="${iconURL}"> </div> </div><br>` +
+        `<div class="description">`+ hell.weather[0].description +`</div>`+ `<br>`+
+        `<div class="details"> DETAILS : </div><br>` +
+        `<div class="feels">Feels Like  </div>` +`<div class="feels-main">`+ hell.main.feels_like + `°`  +`</div>`+
+        `<div class="wind">Wind  </div>` +`<div class="wind-speed">`+ hell.wind.speed + ` mph` +`</div>`+
+        `<div class="humidity">Humidity </div>` +`<div class="humidity-main">`+ hell.main.humidity + `%` +`</div>`+
+        `<div class="pressure">Pressure  </div>` +`<div class="pressure-main">`+ hell.main.pressure + ` hPa` +`</div>`;
+
+}
+
+
+function getAll(city, apiKey) {
+
+    const Forecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=metric"
     return fetch(Forecast)
 
 }
 
-function Current2(varia){
-    console.log(varia)    
+
+function Current2(varia) {
+    console.log(varia)
 
     var content = document.querySelector("#content")
     content.innerHTML = ""
@@ -69,19 +75,26 @@ function Current2(varia){
     const dayInAWeek = new Date().getDay();
     const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
 
-    varia.splice(0,7).map((item, idx)=>{
+
+    varia.splice(0, 7).map((item, idx) => {
+
+
+        var iconCode = item.weather[0].icon;
+        const iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
         var p = document.createElement("p")
-        p.innerHTML =   forecastDays[idx]+ " : " + "<br> "+
-             "Temperature : " + item.main.temp + " °C"+ "<br>" + 
-             "Pressure : "+ item.main.pressure + "<br>" +
-             "Humidity : " + item.main.humidity + "<br>" +
-             "Clouds : " + item.clouds.all + "<br>" + 
-             "Wind Speed : " + item.wind.speed + "<br>" +
-             "Feels Like : " + item.main.feels_like 
-
+        p.innerHTML = `<div class="complete">` + `<div class="days">`+ forecastDays[idx]+ `</div>`  +`<br>` + 
+          `<div class="mixed"><div class="tempe">`+ Math.round(item.main.temp) + `°` + `</div>`+`<div><img class="icon" src="${iconURL}"> </div> </div><br>` + 
+          `<div class="feels-like"> Feels Like : </div>` +  `<div class="feels-like-main">`+ item.main.feels_like + `°`  +  `</div>`+
+         `<div class="wind-speed"> Wind Speed : </div>` + `<div  class="wind-speed-main">`+  item.wind.speed + ` mph` + `</div>`+
+         `<div class="humidity-value"> Humidity : </div>` + `<div class="humidity-value-main">`+  item.main.humidity + `%` + `</div>`+
+         `<div class="pressure-value"> Pressure : </div>` + `<div class="pressure-value-main">`+  item.main.pressure + ` hPa` + `</div>`+
+         `<div class="clouds-cover"> Clouds : </div>` + `<div class="clouds-cover-main">`+ item.clouds.all + `%` + `</div>` + `</div>`
+        
         content.append(p)
     })
+
+
 
 }
 
